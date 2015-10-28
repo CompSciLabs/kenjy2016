333ed88721f050c18d64777b0c36c86ef9d634be
Matthew Elphick <matthew.elphick@clock.co.uk>
2015-10-28 22:28:37 +0000

    > :penguin: :tada: add basic onepage scroll

--- src/js/init.js
+++ src/js/init.js
@@ -1,41 +1,22 @@
 $(document).ready(function () {
-  page.add(
-    { name: 'Home'
-    , element: '.home'
-    }
-  )
-
-  page.add(
-    { name: 'Location'
-    , element: '.location'    
-    }
-  )
-
-  page.add(
-    { name: 'Registry'
-    , element: '.registry'
-    }
-  )
-
-  page.add(
-    { name: 'Schedule'
-    , element: '.schedule'
-    }
-  )
-
-  page.add(
-    { name: 'Gallery'
-    , element: '.gallery'
-    }
-  )
-
-  page.init({ container: '.pages' })
 
   function CountDownHeader(){
     var weddingDay = moment("2016-08-25");
     var Countdown = weddingDay.diff(moment());
     $('.countdown').empty().append( moment(Countdown).format("M,D,H,mm,ss"));
   }
-
+  $(".main").onepage_scroll({
+     sectionContainer: 'section',
+     easing: 'ease',             
+     animationTime: 1000,        
+     pagination: true,           
+     updateURL: false,           
+     beforeMove: function (index) {},
+     afterMove: function (index) {}, 
+     loop: false,                   
+     keyboard: true,                
+     responsiveFallback: false,               
+     direction: 'vertical'
+  });
   window.setInterval(CountDownHeader, 1000);
 })
